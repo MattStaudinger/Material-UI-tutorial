f9c8de62243d9234fe028d5d7916436233be2dd3
Matthias Staudinger <staudinger.matthias@googlemail.com>
2019-01-20 12:12:32 +0100

    Finished Material UI tutorial with theming, cutomizing and Components

--- material-ui-tutorial/src/App.js
+++ material-ui-tutorial/src/App.js
@@ -1,28 +1,126 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Paper, Typography, 
+  TextField, Button, 
+  List, ListItem, ListItemText,
+  ListItemSecondaryAction,  IconButton } from '@material-ui/core';
+import { Delete } from '@material-ui/icons'
+import { withStyles } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { orange } from '@material-ui/core/colors';
+import {  MuiThemeProvider } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: orange
+  }
+})
+
+
+const styles = theme => console.log(theme) || ({
+
+    test: {
+      margin: 20,
+      padding: 20,
+      maxWidth: 400
+    },
+
+    form: {
+      display: 'flex',
+      alignItems: 'baseline',
+      justifyContent: 'space-evenly'
+    },
+    button: {
+      background: "yellow"
+    }
+  })
+
+export default withStyles(styles)(
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       exercises: [
+        { id: 1, title: 'Bench Press' },
+        { id: 2, title: 'Deadlift' },
+        { id: 3, title: 'Squats' }
+      ],
+       title: ""
+    }
+  }
+
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({
+      [name]: value
+    })
+
+
+    handleCreate = e => {
+      e.preventDefault()
+      if (this.state.title) {
+        this.setState(({ exercises, title }) => ({
+          exercises: [
+            ...exercises,
+            {
+              title,
+              id: Date.now()
+            }
+          ],
+          title: ''
+        }))
+      }
+    }
+
+    handleDelete = id =>
+    this.setState(({ exercises }) => ({
+      exercises: exercises.filter(ex => ex.id !== id)
+    }))
+  
   render() {
+    const { title, exercises } = this.state;
+    const {classes} = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+      <Paper className={classes.test}  >
+      <Typography variant="display2" align="center" gutterBottom>Exercises</Typography>
+      <form className={classes.form} onSubmit={this.handleCreate}>
+        <TextField
+          name='title'
+          label='Exercise'
+          value={title}
+          onChange={this.handleChange}
+          margin='normal'
+        />
+        <Button
+          className={classes.button}
+          type='submit'
+          color='primary'
+          variant='contained'
+        >Create
+        </Button>
+      </form>
+
+       <List>
+        {exercises.map(({ id, title }) =>
+          <ListItem key={id}>
+            <ListItemText primary={title} />
+            <ListItemSecondaryAction>
+              <IconButton
+                color='primary'
+                onClick={() => this.handleDelete(id)}
+              >
+                 <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        )}
+      </List>
+
+      </Paper>
+      </MuiThemeProvider>
     );
   }
 }
+)
 
-export default App;
---
+++ material-ui-tutorial/src/List.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Button, 
+   } from '@material-ui/core';
+
+export default class List extends Component {
+  render() {
+    return (
+      <div>
+<Button
+          type='submit'
+          color='primary'
+          variant='contained'
+        >TEEEST
+        </Button>      </div>
+    )
+  }
+}
